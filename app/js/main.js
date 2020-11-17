c5ff205272529dc6361c2c4f6567576fd7c4cce4
Natalia Bannikova <natabannikova10@gmail.com>
2020-11-17 23:17:14 +0200

    Add slider-dots

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,15 @@ $(function(){
 $('.header__slider').slick({
     infinite: true,
     fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow-left">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow-right">',
+    asNavFor: '.slider-dots',
 });
 
-  
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+  })
+
 });
